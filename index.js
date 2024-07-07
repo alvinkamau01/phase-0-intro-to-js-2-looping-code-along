cf550e9f021e64a81dcb2325de10c12704fa70a4
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-07-07 06:20:34 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let value=10
+function countDown(value){
+   while(value>-1){
+   console.log(value--)
+   }
+   return value
+}
+
+function writeCards(names){
+    for(let person of names){
+     console.log`${person}`
+     }
+     const Messages = names.map(person => `Thank you, ${person}, for the wonderful surprise gift!`)
+     return Messages
+    }
